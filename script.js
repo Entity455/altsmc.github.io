1ea14496e5d55fcd973c635391f56756238c9764
Tymon Rojczyk <rojogamesyt@gmail.com>
2022-01-12 16:28:14 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let baza = new Array(9);
+    baza[0] = "markasgmarkas@gmail.com:sunus2009";
+    baza[1] = "aliyapadilla11@gmail.com:fairywings11";
+    baza[2] = "asmaasfour1991@gmail.com:asma1991";
+    baza[3] = "mmmm7770120@gmail.com:mt01201192";
+    baza[4] = "it.student1029@gmail.com:kazu1029";
+    baza[5] = "tomtim83520@gmail.com:kp9ixhd2";
+    baza[6] = "yumesaku0123@gmail.com:ryuya0203";
+    baza[7] = "tladudqls26@gmail.com:steve@031126";
+    baza[8] = "kinsamv@gmail.com:lmaohoe1234";
+    baza[9] = "kinsamv@gmail.com:lmaohoe1234";
+
+    function losuj(){
+        document.write(baza[Math.floor(Math.random()*5)]);
+    };
